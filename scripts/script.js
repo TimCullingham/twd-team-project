9bdc91c6a28ae918d6cd7b3b4c261b8f4919a7b4
Sonam-Jaiswal <56893128+Sonam-Jaiswal@users.noreply.github.com>
2019-11-12 12:34:39 -0800

    header changes

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+ //Open Hamburger Nav
+ function openNav() {
+    document.getElementById('nav').style.width = '100%';
+  }
+  
+  //Close Hamburger Nav
+  function closeNav() {
+    document.getElementById('nav').style.width = '0%';
+  }
+  
+  //Event Listeners
+  document.getElementsByClassName('hamburger')[0].addEventListener('click', openNav);
+  document.getElementsByClassName('closeBtn')[0].addEventListener('click', closeNav);
